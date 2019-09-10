2eac92dc45122ffc005db527bd3894d7fbc9b52d
ebhamersma <ebhamersma@gmail.com>
2019-09-10 17:54:04 +1000

    Pause Screen
    Made it so if the player presses the escape key then the game will be paused with the options to go to the main menu or return back to the game as it was before they left it

---
+++ Term 3 Assessment/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGame : MonoBehaviour
+{
+    public string MainMenu;
+
+    public GameObject thePauseScreen;
+
+    private PlayerController thePlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thePlayer = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //makes it so depending on if the time.timescale = 1 or 0 depends on if it runs pausegame or resume game
+        if (Input.GetButtonDown("Pause"))
+        {
+            if (Time.timeScale == 0f)
+            {
+                ResumeGame();
+            }
+
+            else
+            {
+                GamePause();
+            }
+        }
+    }
+    //makes it so if the escape key is pressed down then the pause screen will be activated and the everything in the world will stop moving, and will pause music
+    public void GamePause()
+    {
+        Time.timeScale = 0;
+
+        thePauseScreen.SetActive(true);
+        thePlayer.canMove = false;
+    }
+
+    public void ResumeGame()
+    {
+        //makes it so if the user clicks the resume game button when paused then the pausescreen will be deactivated and eveything in the world will be able to move again, and will start playing music again
+        thePauseScreen.SetActive(false);
+        thePlayer.canMove = true;
+
+        Time.timeScale = 1f;
+    }
+
+    public void QuittoMainMenu()
+    { 
+        SceneManager.LoadScene(MainMenu);
+    }
+}
