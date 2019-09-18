f9570c5decd85eb6d0cffcf2157d9fbeaf5b08b8
Lenni <XiongHelen@outlook.com>
2019-09-18 12:45:16 +1000

    Nothing
    just trying to pull

--- Term 3 Assessment/Assets/Scripts/EnemyController.cs
+++ Term 3 Assessment/Assets/Scripts/EnemyController.cs
@@ -36,12 +36,14 @@ public class EnemyController : MonoBehaviour
         //moves the enemy in the direction it is facing
         transform.position += transform.up * speed;
 
+        //states that if the enemy health is equal or below 0 it destroys/deletes itself
         if (currentHealth <= 0)
         {
             Destroy(gameObject);
         }
     }
 
+    //When the enemy collides with a player bullet it takes away one health
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
@@ -57,6 +59,7 @@ public class EnemyController : MonoBehaviour
         }
     }
 
+    //This states that enemy fire routine, the enemy will fire at random times and wait 2-4secs before firing at the player again.
     IEnumerator EnemyBulletCycle()
     {
         while (true)
